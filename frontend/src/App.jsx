05b3e274e6927125a66d0fe7f79e8e502aeb940c
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2026-02-12 14:21:20 +0000

    Installed react and setup tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-eco-dark h-screen w-2 f">
+      <p className="text-3xl font-bold text-eco-accent">Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
