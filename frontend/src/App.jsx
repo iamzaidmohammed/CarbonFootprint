527d55243a2bdad0bba2dd917f8ff14551ee31fc
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2026-04-21 16:23:24 +0000

    feat: Added a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,35 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 import History from "./pages/History";
+import Navbar from "./components/Navbar";
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+// Then add <ScrollToTop /> inside your <Router> in App.jsx
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/results/:id" element={<Results />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
+      <ScrollToTop />
+      <div className="min-h-screen bg-eco-dark text-white font-sans">
+        <Navbar /> {/* This stays at the top */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/results/:id" element={<Results />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
